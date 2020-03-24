2cc8f3933aff00c29a19e26c90fe8871d87ebcb5
Kasey Colian <kaseycolian@gmail.com>
2020-03-24 18:24:38 -0400

    Setup started

---
+++ about_extracting_page_elements.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require File.expand_path(File.dirname(__FILE__) + '/edgecase')
+require File.expand_path(File.dirname(__FILE__) + '/browser')
+
+require 'selenium-webdriver'
+
+class AboutExtractingPageElements < Edgecase::Koan
+
+  def test_you_can_extract_the_page_title
+    on_browserkoans_test_page do |driver|
+      page_title = driver.title
+      assert_match __, page_title
+    end
+  end
+
+  def test_you_can_extract_the_page_source
+    on_browserkoans_test_page do |driver|
+      page_source = driver.page_source
+      assert_match __, page_source
+    end
+  end
+
+  
+  def test_you_can_get_the_contents_of_a_found_element
+    on_browserkoans_test_page do |driver|
+      paragraph_text = driver.find_element(id: 'mypara').text
+      assert_match __, paragraph_text
+    end
+  end
+    
+  def test_we_can_get_other_attributes_in_addition_to_text
+    on_browserkoans_test_page do |driver|
+      element = driver.find_element(css: ".p-with-link a")
+      href = element.attribute("href")
+      protocol = element.attribute("protocol")
+
+      assert_equal __, href
+      assert_equal __, protocol
+    end
+  end
+
+end
---
+++ about_waiting.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require File.expand_path(File.dirname(__FILE__) + '/edgecase')
+require File.expand_path(File.dirname(__FILE__) + '/browser')
+
+require 'selenium-webdriver'
+
+class AboutWaiting < Edgecase::Koan
+
+  # Many, if not most, web applications now use javascript to provide
+  # a more dynamic experience. When extracting page elements, we need
+  # to be mindful that we might have to wait for a change to occur before
+  # we can reason about what is on the page.
+  def test_webdriver_does_not_wait_fory_dynamic_page_changes_by_default
+    on_browserkoans_test_page do |driver|
+      volatile_text = -> { driver['delayed-text'].text }
+      assert_equal __, volatile_text.call[/Loading/].nil?
+      assert_equal __, volatile_text.call[/jQuery/].nil?
+    end
+  end
+
+  def wait_for(timeout,&block)
+    wait = Selenium::WebDriver::Wait.new(:timeout => timeout)
+    wait.until { yield }
+  end
+
+  def test_wait_for_the_text_to_change
+    saw_change = false
+    on_browserkoans_test_page do |driver|
+      volatile_text = -> { driver['delayed-text'].text }
+      begin
+        wait_for(10) { !volatile_text.call[/Loading/] }
+        saw_change = true
+      rescue Selenium::WebDriver::Error::TimeoutError => ex
+      end
+    end
+    assert_equal __, saw_change
+  end
+
+  def test_wait_but_not_for_long_enough
+    saw_change = false
+    on_browserkoans_test_page do |driver|
+      volatile_text = -> { driver['delayed-text'].text }
+      assert_raise(__) do
+        wait_for(1) { !volatile_text.call[/Loading/] }
+        saw_change = true
+      end
+    end
+    assert_equal __, saw_change
+  end
+end
